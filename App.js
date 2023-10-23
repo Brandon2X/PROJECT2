916a5374f8faab436d0fcdad6b8b94efdcec61ab
Brandon2X <bolivencia02@gmail.com>
2023-10-23 18:32:35 -0400

    Updated our game to use JSX

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 function App() {
     return (
+      
     <div>
     <h1>Tic-React-Toe</h1>
-    </div>
-    )
+    <div class="flex-container flex-column">
+        <div class="flex-container flex-wrap" id="board">
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <button id="reset-button">reset</button>
+        <p>If you want to learn how I made this game happen click  <a href=https://www.youtube.com/watch?v=oZrp3Atkz18&t=378s>here.</a></p>
+    
+       
+    
+       
+       
+        
+       
+    
+     )
     }
-    ReactDOM.render(<App />, root)  
-      
+    
+  
+
