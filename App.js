b26b7d1d7a15f6ac7ee68983239a22e796ea705d
Brandon2X <bolivencia02@gmail.com>
2023-10-24 12:50:44 -0400

    Redo event logic to work with react components

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 function App() {
+  const [board, setBoard] = React.useState(["","","","","","","","",""])
+
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
+    
+let turn = 'X';
+let win;
+function handleTurn(event) {
+  console.log(event.target, event.target.id)
+  let idx = event.target.id
+  if (gameOver == false) {
+  let newBoard = [...board]
+  newBoard[idx] = turn
+  setBoard(newBoard)
+  turn = turn === 'X' ? 'O' : 'X'
+  // win = getWinner()
+  // render()
+  }
+  }
+
+
+
     return (
       
+    
     <div>
-    <h1>Tic-React-Toe</h1>
-    <div class="flex-container flex-column">
-        <div class="flex-container flex-wrap" id="board">
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <div class="square"></div>
-        <button id="reset-button">reset</button>
-        <p>If you want to learn how I made this game happen click  <a href=https://www.youtube.com/watch?v=oZrp3Atkz18&t=378s>here.</a></p>
+<h1>Tic-React-Toe</h1>
+<h2>It's X's turn!</h2>
+<div class="flex-container flex-column">
+<div class="flex-container flex-wrap" id="board" onClick={handleTurn}>
+{board.map((value, idx) => {
+return (
+<div class="square" key={idx} id={idx}>
+{value}
+</div>
+)
+})}
+</div>
+<button id="reset-button">reset</button>
+</div>
+</div>
     
        
     
