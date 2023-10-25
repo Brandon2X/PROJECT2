2c7f71fbb65ea88acd110be68d41301dd0871105
Brandon2X <bolivencia02@gmail.com>
2023-10-25 16:06:46 -0400

    this is the revised and final version

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 function App() {
-  const [board, setBoard] = React.useState(["","","","","","","","",""])
+  const [board, setBoard] = React.useState(Array(9).fill(""));
+  const [gameOver, setGameOver] = React.useState(false);
+  const [turn, setTurn] = React.useState('X');
+  const [winner, setWinner] = React.useState(null); // Add this line
 
   const winningCombos = [
     [0, 1, 2],
@@ -10,55 +13,60 @@ function App() {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-    ];
+  ];
 
-    
-let turn = 'X';
-let win;
-function handleTurn(event) {
-  console.log(event.target, event.target.id)
-  let idx = event.target.id
-  if (gameOver == false) {
-  let newBoard = [...board]
-  newBoard[idx] = turn
-  setBoard(newBoard)
-  turn = turn === 'X' ? 'O' : 'X'
-  // win = getWinner()
-  // render()
-  }
+  // Add this function to check for a winner
+  function checkWinner(board) {
+    for (let combo of winningCombos) {
+      if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+        return board[combo[0]];
+      }
+    }
+    return null;
   }
 
+  function handleTurn(event) {
+    let idx = event.target.id;
+    if (!gameOver && !board[idx]) {
+      let newBoard = [...board];
+      newBoard[idx] = turn;
+      setBoard(newBoard);
+      let nextTurn = turn === 'X' ? 'O' : 'X';
+      setTurn(nextTurn);
+      let winner = checkWinner(newBoard);
+      if (winner) {
+        setGameOver(true);
+        setWinner(winner);
+      }
+    }
+  }
 
+  // Add this function to reset the game
+  function resetGame() {
+    setBoard(Array(9).fill(""));
+    setGameOver(false);
+    setTurn('X');
+    setWinner(null);
+  }
 
-    return (
-      
-    
-    <div>
+return (
+<div>
 <h1>Tic-React-Toe</h1>
-<h2>It's X's turn!</h2>
-<div class="flex-container flex-column">
-<div class="flex-container flex-wrap" id="board" onClick={handleTurn}>
+<h2>{gameOver ? `Game Over! ${winner} is the winner!` : `It's ${turn}'s turn!`}</h2>
+<div className="flex-container flex-column">
+<div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
 {board.map((value, idx) => {
 return (
-<div class="square" key={idx} id={idx}>
+<div className="square" key={idx} id={idx}>
 {value}
 </div>
 )
 })}
 </div>
-<button id="reset-button">reset</button>
+<button id="reset-button" onClick={resetGame}>reset</button> {/* Modify this line */}
 </div>
 </div>
-    
-       
-    
-       
-       
-        
-       
-    
-     )
-    }
-    
-  
+);
+}
 
+ReactDOM.render(<App/>, document.getElementById('root'));
